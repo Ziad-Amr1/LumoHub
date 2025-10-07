50a68cfb190e7a44ec40c090cf4d50fff5f51a74
Ziad Amr <Zyadamr177@gmail.com>
2025-10-07 14:35:55 +0300

    Updates

--- assets/star-D6mxcWoG.js
+++ assets/star-D6mxcWoG.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DBmFzG49.js";/**
+import{c as a}from"./index-D-pYbCzW.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
