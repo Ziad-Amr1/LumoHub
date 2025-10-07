a3a9761ab5a430f1d64c1d227dd12e97be412044
Ziad Amr <Zyadamr177@gmail.com>
2025-10-07 14:33:40 +0300

    chore: wrap app with ProfileProvider and refine Layout/App structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -69,7 +69,7 @@ export default function App() {
             }
           />
           <Route
-            path="/movie/:id"
+            path="/movies/:id"
             element={
               <Layout>
                 <MovieDetails />
