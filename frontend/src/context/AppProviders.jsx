2c51a2c52690b9364c362ef04e40812d457db764
Ziad Amr <Zyadamr177@gmail.com>
2025-10-07 14:33:39 +0300

    feat: add ProfileProvider to AppProviders for global profile state

--- frontend/src/context/AppProviders.jsx
+++ frontend/src/context/AppProviders.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "./ThemeContext";
 import { AuthProvider } from "./AuthContext";
 import { PageLoaderProvider } from "./PageLoaderContext";
 import { MovieActionsProvider } from "./MovieActionsContext";
+import { ProfileProvider } from "./ProfileContext";
 
 export function AppProviders({ children }) {
   return (
@@ -11,7 +12,9 @@ export function AppProviders({ children }) {
       <AuthProvider>
         <PageLoaderProvider>
           <MovieActionsProvider>
-            {children}
+            <ProfileProvider>
+              {children}
+            </ProfileProvider>
           </MovieActionsProvider>
         </PageLoaderProvider>
       </AuthProvider>
